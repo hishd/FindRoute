3b1e273e89810374cbf4e13d63aa6bf184f837b5
cobsccomp192p-014 <35591927+hishd@users.noreply.github.com>
2022-08-21 08:53:31 +0530

    Initial Commit

--- FindRoute/FindRouteApp.swift
+++ FindRoute/FindRouteApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FindRouteApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RouteView()
         }
     }
 }
--- FindRoute/RouteView.swift
+++ FindRoute/RouteView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RouteView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RouteViewPreviews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
