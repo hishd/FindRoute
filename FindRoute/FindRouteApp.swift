a866857edba186e6c399c4ab57d55f216f0e3971
cobsccomp192p-014 <35591927+hishd@users.noreply.github.com>
2022-08-21 08:51:46 +0530

    Initial Commit

---
+++ FindRoute/FindRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindRouteApp.swift
+//  FindRoute
+//
+//  Created by Hishara Dilshan on 2022-08-21.
+//
+
+import SwiftUI
+
+@main
+struct FindRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
